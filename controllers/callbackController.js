deeb67b0d54caf8bcfab4345332636fa066c067b
wwiarenicz <wwiarenicz@gmail.com>
2023-06-08 21:27:28 +0200

    ok

--- controllers/callbackController.js
+++ controllers/callbackController.js
@@ -1,14 +1,18 @@
 const { makeOrder } = require('../services/orderService')
 function initializeCallback(bot) {
-    bot.on('callback_query', (msg) => {
+    bot.on('callback_query', async (msg) => {
         const uid = msg.from.id
         const query = msg.data
         const q_method = query.split('_')[0]
-        console.log(query)
         if (q_method === 'portion') {
             const q_value = (query.split(',')[0]).split('_')[1]
             const pid = (query.split(',')[1]).split('_')[1]
-            const response = makeOrder(uid, q_value, pid)
+            const response = await makeOrder(uid, q_value, pid)
+            if (response.status === 200) {
+                bot.sendMessage(uid, response.message, { parse_mode: 'HTML' })
+            } else {
+                bot.sendMessage(uid, response.message, { parse_mode: 'HTML' })
+            }
         } else {
             bot.sendMessage(uid, `<i>Unknown command\nGo back to /start</i>`, { parse_mode: 'HTML' })
         }
--- services/orderService.js
+++ services/orderService.js
@@ -4,16 +4,37 @@ async function makeOrder(tgid, config, pid) {
     const uid = await getUid(tgid)
     const customerActiveOrder = `SELECT * FROM orders WHERE user_id = $1 AND is_completed = false`
     const response = await pool.query(customerActiveOrder, [uid])
+    console.log(response)
+    const query = `
+        INSERT INTO order_items (order_id, product_id, quantity, price_per_unit) 
+        VALUES($1, $2, $3, $4)`
     if (response.rows.length > 0) {
         //user has opened unpayed order
         const actualOrder = response.rows[0]
         const orderId = actualOrder.id
         console.log(`We are about to add ${config} of PID${pid} to OID ${orderId}`)
-        const query = `INSERT INTO order_items (order_id, product_id, quantity, price_per_unit) VALUES($1, $2, $3, $4)`
-        const addDoOrderResponse = await pool.query(query, [orderId, pid, 1, 228.00])
-        console.log(addDoOrderResponse)
+        try {
+            const addDoOrderResponse = await pool.query(query, [orderId, pid, 1, 228.00])
+            return { status: 200, message: `Item has been added to the order #${orderId}. \n To procceed order process, please go back to /start \n and choose "Unfinished order..."` }
+        } catch (error) {
+            return { status: 500, message: "Error placing order, contact the staff... /start" }
+        }
+
     } else {
-        //user has no opened unpayed orders
+        //user has no opened unpayed order
+        const createOrder = `
+        INSERT INTO orders (user_id, date, total_price, delivery_type, delivery_address, delivery_price, deadline, is_completed)
+        VALUES($1, CURRENT_TIMESTAMP, 0.00, null, null, null, null, false)
+        RETURNING id;
+        `
+        try {
+            const createOrderRes = await pool.query(createOrder, [uid])
+            const orderId = createOrderRes.rows[0].id
+            const addToOrder = await pool.query(query, [orderId, pid, 1, 228])
+            return { status: 200, message: `New order #${orderId} has been created. Item has been added to the order. \n To procceed, please go back to /start \n and choose "Unfinished order..."` }
+        } catch (error) {
+            return { status: 500, message: "Error placing new order, contact the staff... /start" }
+        }
     }
 }
 async function ordersCustomer(uid) {
